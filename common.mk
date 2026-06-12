7c7f2228fc3ff560ee937f9f88e140527c4528f1
Musafir02 <askibrahim06@gmail.com>
2026-06-12 13:59:26 +0000

    add support for sony dolby vision

--- common.mk
+++ common.mk
@@ -25,6 +25,10 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener
 
+# Sony Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
